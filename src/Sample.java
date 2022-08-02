4df5de61b48a85368602f3be6930e9f5c627b214
신희진 <heejinshin02@gmail.com>
2022-08-02 11:47:32 +0900

    first commit

---
+++ src/Sample.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Sample {
+
+    public static void main(String[] args) {
+
+        String str = "홍길동";
+        System.out.println(str);
+
+        Date today = Calendar.getInstance().getTime();
+        System.out.println(today);
+    }
+}
